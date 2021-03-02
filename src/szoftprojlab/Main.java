199d8aef0d04b6324dc865c641ac1fc98fc2e119
Roland Nagy <roland.nagy@proglimes.com>
2021-03-02 21:03:18 +0100

    hello world for intellij

--- src/szoftprojlab/Main.java
+++ src/szoftprojlab/Main.java
@@ -1,7 +1,7 @@
 package szoftprojlab;
 
 class Main {
-	public static void main(String[] args) {
-		System.out.println("Hello World?");
-	}
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 }
